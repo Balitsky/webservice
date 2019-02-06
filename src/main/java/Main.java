d4c719572d9cf4287667181ef21f854b320904fc
Balitsky <balitsky99@gmail.com>
2019-02-06 13:26:07 +0300

    start server, Get request

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
